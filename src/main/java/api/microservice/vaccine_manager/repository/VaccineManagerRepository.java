0f8e5f3fc60d34d24c7535a8cf7f1900439525ba
Sérgio Filho <fragasergio23@gmail.com>
2023-10-17 22:07:55 -0300

    Fix Repository

--- src/main/java/api/microservice/vaccine_manager/repository/VaccineManagerRepository.java
+++ src/main/java/api/microservice/vaccine_manager/repository/VaccineManagerRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface VaccineManagerRepository extends MongoRepository<VaccineManager, String> {
-    List<VaccineManager> findAllByState(String estado);
 }
